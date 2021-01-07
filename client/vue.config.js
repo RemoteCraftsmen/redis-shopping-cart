040c42f2e6fc7912d01f105e4584b2dd02b8097b
Lukasz Bien <lukasz.bien@remotecraftsmen.com>
2021-01-07 14:04:17 +0100

    Fixes

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,20 +1,19 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  "configureWebpack": {
-    "resolve": {
-      "alias": {
-        "@": "/Users/lukasz/Projects/Other/redis-shopping-cart/client/src"
-      }
+    transpileDependencies: ['vuetify'],
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@':
+                    '/Users/lukasz/Projects/Other/redis-shopping-cart/client/src'
+            }
+        }
+    },
+    pluginOptions: {
+        i18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: true
+        }
     }
-  },
-  "pluginOptions": {
-    "i18n": {
-      "locale": "en",
-      "fallbackLocale": "en",
-      "localeDir": "locales",
-      "enableInSFC": true
-    }
-  }
-}
+};
