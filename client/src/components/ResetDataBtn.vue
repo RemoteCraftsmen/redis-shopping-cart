0f022fe681aa4f7ac05a13926ee50a711e7cb06e
Artur Książek <artur.ksiazek@remotecraftsmen.com>
2021-02-05 17:54:03 +0100

    Front-end Redesign

--- client/src/components/ResetDataBtn.vue
+++ client/src/components/ResetDataBtn.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-btn v-if="cartItems.length" class="mx-auto error" @click="resetData">
+    <v-btn class="mx-auto error" @click="resetData">
         Reset data
         <v-icon right dark>mdi-restore</v-icon>
     </v-btn>
